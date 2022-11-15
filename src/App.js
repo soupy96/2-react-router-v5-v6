773f854c698a7df104f312132c4a859345476c91
Michael Campbell <michael.campbell14@gmail.com>
2022-11-15 13:57:14 -0500

    correcting Redirects and fixing nested routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, Redirect } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -11,10 +11,8 @@ function App() {
       <MainHeader />
       <main>
         <Routes>
-          <Route path='/' exact>
-            <Redirect to='/welcome' />
-          </Route>
-          <Route path='/welcome' element={<Welcome />} />
+          <Route path='/' element={<Navigate replace to='/welcome' />} />
+          <Route path='/welcome/*' element={<Welcome />} />
           <Route path='/products' element={<Products />} />
           <Route path='/products/:productId' element={<ProductDetail />} />
         </Routes>
