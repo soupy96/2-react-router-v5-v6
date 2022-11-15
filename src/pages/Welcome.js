4689ab022fe607eebaeb88901658764071d628d2
Michael Campbell <michael.campbell14@gmail.com>
2022-11-15 14:04:19 -0500

    CHORE moving all nested routes to App.js and use Outlet to output the pages content

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,12 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
-      <Routes>
-        <Route path='/new-user' element={<p>Welcome, new user!</p>} />
-      </Routes>
+      <Outlet />
     </section>
   );
 };
